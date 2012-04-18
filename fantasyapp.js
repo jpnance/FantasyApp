68f944c52b9ab78b06cd53e5a4b9f87b87e35db5
Patrick Nance <jpnance@gmail.com>
2012-04-17 21:32:48 -0700

    Fixing some relatively clerical things in the name of extending the Chrome extension to Yahoo!.

--- fantasyapp.js
+++ fantasyapp.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-	var espnLauncher = $('li.games-fancast-btn a, li.statracker a');
+	var launcher = $('li.games-fancast-btn a, li.statracker a');
 
-	espnLauncher.attr('onclick', '').unbind('click');
-	espnLauncher.click(function(e) {
+	launcher.attr('onclick', '').unbind('click');
+	launcher.click(function(e) {
 		var $this = $(this);
 		var modalDiv = $('<div id="modal">');
-		var iframe = $('<iframe id="fantasyapp" class="fantasycast">');
+		var iframe = $('<iframe id="fantasyapp">');
 		iframe.attr('src', $this.attr('href'));
 
 		$('body').append(modalDiv).append(iframe).css('overflow', 'hidden');
