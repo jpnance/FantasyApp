c4378dc79ba44d233cb4b29900f988833932142b
Patrick Nance <jpnance@gmail.com>
2012-04-17 22:03:15 -0700

    Simple optimization so we don't do stuff unless we actually have an appropriate link to operate on.

--- fantasyapp.js
+++ fantasyapp.js
@@ -1,20 +1,22 @@
 $(document).ready(function() {
 	var launcher = $('li.games-fancast-btn a, li.statracker a');
 
-	launcher.attr('onclick', '').unbind('click');
-	launcher.click(function(e) {
-		var $this = $(this);
-		var modalDiv = $('<div id="modal">');
-		var iframe = $('<iframe id="fantasyapp">');
-		iframe.attr('src', $this.attr('href'));
+	if (launcher.length > 0) {
+		launcher.attr('onclick', '').unbind('click');
+		launcher.click(function(e) {
+			var $this = $(this);
+			var modalDiv = $('<div id="modal">');
+			var iframe = $('<iframe id="fantasyapp">');
+			iframe.attr('src', $this.attr('href'));
 
-		$('body').append(modalDiv).append(iframe).css('overflow', 'hidden');
+			$('body').append(modalDiv).append(iframe).css('overflow', 'hidden');
 
-		return false;
-	});
+			return false;
+		});
 
-	$('body').delegate('#modal', 'click', function(e) {
-		$('#fantasyapp, #modal').remove();
-		$('body').css('overflow', 'auto');
-	});
+		$('body').delegate('#modal', 'click', function(e) {
+			$('#fantasyapp, #modal').remove();
+			$('body').css('overflow', 'auto');
+		});
+	}
 });
